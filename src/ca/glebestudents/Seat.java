6c167f7a5387976c2041f0d83eb46f92753d37c1
WilliamHuang24 <williamhuang24@gmail.com>
2019-11-25 13:38:51 -0500

    Row and Seat class

---
+++ src/ca/glebestudents/Seat.java
@@ -0,0 +1,15 @@
+package ca.glebestudents;
+
+import javafx.scene.paint.Color;
+
+public class Seat {
+	private Color c;
+	
+	public Seat(Color color) {
+		c = color;
+	}
+	
+	public Color getColor() {
+		return c;
+	}
+}
